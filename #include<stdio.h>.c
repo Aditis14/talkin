5dff6b727f6f5d4e583ae6847aa7bf32de356e45
Aditis14 <68690277+Aditis14@users.noreply.github.com>
2021-08-27 14:25:25 +0530

    Talkin

---
+++ #include<stdio.h>.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+ int a, b, sum=0;
+ printf("PLEASE ENTER THE VALUES OF A AND B:\n");
+ scanf("%d %d",&a, &b);
+    sum=a+b;
+  printf("the sum is %d\n", sum);
+    return 0;
+ 
